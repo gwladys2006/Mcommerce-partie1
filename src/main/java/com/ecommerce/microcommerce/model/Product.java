656b2ef3e83f985caf94c9d1b6e6108af63eaf00
Laura Koch <laura.koch@viseo.com>
2019-06-06 14:27:38 +0200

    Partie 3 - Validation du prix de vente

--- src/main/java/com/ecommerce/microcommerce/model/Product.java
+++ src/main/java/com/ecommerce/microcommerce/model/Product.java
@@ -3,7 +3,6 @@ package com.ecommerce.microcommerce.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.validation.constraints.Min;
 
 import org.hibernate.validator.constraints.Length;
 
@@ -18,7 +17,6 @@ public class Product {
     @Length(min=3, max=20, message = "Nom trop long ou trop court. Et oui messages sont plus stylés que ceux de Spring")
     private String nom;
 
-    @Min(value = 1)
     private int prix;
 
     //information que nous ne souhaitons pas exposer
--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.ecommerce.microcommerce.web.controller;
 
 import com.ecommerce.microcommerce.dao.ProductDao;
 import com.ecommerce.microcommerce.model.Product;
+import com.ecommerce.microcommerce.web.exceptions.ProduitGratuitException;
 import com.ecommerce.microcommerce.web.exceptions.ProduitIntrouvableException;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
@@ -54,8 +55,13 @@ public class ProductController {
     public ResponseEntity<Void> ajouterProduit(@Valid @RequestBody Product product) {
         Product productAdded =  productDao.save(product);
         
-        if (productAdded == null)
+        if (productAdded == null) {
             return ResponseEntity.noContent().build();
+        }
+        
+        if (product.getPrix() <= 0) {
+        	throw new ProduitGratuitException("Le prix de vente doit être supérieur à zéro.");
+        }
         
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
